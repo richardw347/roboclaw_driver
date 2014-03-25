e07d056b3fb3a5f3f89fac5d04541c8c0ad689e4
Richard-Lab <richardw347@gmail.com>
2014-03-25 09:43:02 +0000

    odometry publishing workin

--- src/RoboclawNode.cpp
+++ src/RoboclawNode.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <diagnostic_msgs/DiagnosticArray.h>
@@ -9,31 +10,31 @@
 
 class RoboclawNode{
 public:
-    RoboclawNode(): nh("~"){
-        nh.param<std::string>("port", port, "/dev/ttyACM0");
-        nh.param<std::string>("base_frame_id", base_frame_id, "base_link");
-        if(!nh.getParam("baud_rate", baud_rate)){
+    RoboclawNode(): nh(), priv_nh("~"){
+        priv_nh.param<std::string>("port", port, "/dev/ttyACM0");
+        priv_nh.param<std::string>("base_frame_id", base_frame_id, "base_link");
+        if(!priv_nh.getParam("baud_rate", baud_rate)){
             baud_rate = 38400;
         }
-        if(!nh.getParam("rate", update_rate)){
+        if(!priv_nh.getParam("rate", update_rate)){
             baud_rate = 30;
         }
-        if(!nh.getParam("base_width", base_width)){
+        if(!priv_nh.getParam("base_width", base_width)){
             baud_rate = 0.5;
         }
-        if(!nh.getParam("ticks_per_metre", ticks_per_m)){
+        if(!priv_nh.getParam("ticks_per_metre", ticks_per_m)){
             baud_rate = 100;
         }
-        if(!nh.getParam("KP", KP)){
+        if(!priv_nh.getParam("KP", KP)){
             KP = 0.1;
         }
-        if(!nh.getParam("KI", KI)){
+        if(!priv_nh.getParam("KI", KI)){
             KI = 0.5;
         }
-        if(!nh.getParam("KD", KD)){
+        if(!priv_nh.getParam("KD", KD)){
             KD = 0.25;
         }
-        if(!nh.getParam("QPPS", QPPS)){
+        if(!priv_nh.getParam("QPPS", QPPS)){
             QPPS = 1000;
         }
 
@@ -59,9 +60,9 @@ public:
         odom.child_frame_id = base_frame_id;
         odom.pose.pose.position.z = 0.0;
 
-        cmd_vel_sub = nh.subscribe("/cmd_vel", 10, &RoboclawNode::twistCb, this);
-        diag_pub = nh.advertise<diagnostic_msgs::DiagnosticArray>("/diagnostics", 10);
-        odom_pub = nh.advertise<nav_msgs::Odometry>("/odom", 10);
+        cmd_vel_sub = nh.subscribe("cmd_vel", 10, &RoboclawNode::twistCb, this);
+        diag_pub = nh.advertise<diagnostic_msgs::DiagnosticArray>("diagnostics", 10);
+        odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 10);
 
         claw->SetM1VelocityPID(KD,KP,KI,QPPS);
         claw->SetM2VelocityPID(KD,KP,KI,QPPS);
@@ -71,6 +72,72 @@ public:
 
         ROS_INFO_STREAM("Connected to: " << roboclaw_version);
    }
+
+    void upateOdom(){
+
+        // calcuate time elapsed since last update
+        ros::Time now = ros::Time::now();
+        ros::Duration elapsed_t = now - last_odom;
+        last_odom = now;
+        double elapsed = elapsed_t.toSec();
+
+        uint8_t status;
+        bool valid1, valid2;
+
+        // read the encoder counts
+        long encoder_left = claw->ReadEncoderM1(status, valid1);
+        long encoder_right = claw->ReadEncoderM2(status, valid2);
+
+        if (!valid1 || !valid2){
+            ROS_WARN_STREAM("Invalid encoder count reading");
+            return;
+        }
+
+        // calculate distance travelled by each wheel
+        double dist_left = (float)(encoder_left - last_enc_left) / ticks_per_m;
+        double dist_right = (float)(encoder_right - last_enc_right) / ticks_per_m;
+
+        last_enc_left = encoder_left;
+        last_enc_right = encoder_right;
+
+        // distance robot has travelled is the average of the distance travelled by both
+        // wheels
+        double dist_travelled = (dist_left + dist_right) / 2;
+        // calculate appoximate heading change (radians), this works for small angles
+        double delta_th = (dist_right - dist_left) / base_width;
+        double vx = dist_travelled / elapsed;
+        double vth = delta_th / elapsed;
+
+        if (dist_travelled != 0){
+            double delta_x = cos(delta_th) * dist_travelled;
+            double delta_y = -sin(delta_th) * dist_travelled;
+
+            x += (cos(theta) * delta_x - sin(theta) * delta_y);
+            y += (sin(theta) * delta_x + cos(theta) * delta_y);
+        }
+
+        if (delta_th != 0){
+            theta += delta_th;
+        }
+
+        tf::Transform transform;
+        transform.setOrigin(tf::Vector3(x,y,0.0));
+        tf::Quaternion q;
+        q.setRPY(0.0,0.0,theta);
+        transform.setRotation(q);
+        br.sendTransform(tf::StampedTransform(transform, now, "odom", base_frame_id));
+
+        odom.header.stamp = now;
+        odom.pose.pose.position.x = x;
+        odom.pose.pose.position.y = y;
+        odom.pose.pose.orientation.x = odom.pose.pose.orientation.y = 0;
+        odom.pose.pose.orientation.z = sin(theta/2);
+        odom.pose.pose.orientation.w = cos(theta/2);
+        odom.twist.twist.linear.x = vx;
+        odom.twist.twist.angular.z = vth;
+        odom_pub.publish(odom);
+
+    }
 
     void twistCb(geometry_msgs::Twist msg){
         last_motor = ros::Time::now();
@@ -92,17 +159,15 @@ public:
         ros::Rate r(update_rate);
         while (ros::ok()){
             ros::spinOnce();
-
-
+            this->upateOdom();
             r.sleep();
-
         }
         this->shutdown();
     }
 
 
 private:
-    ros::NodeHandle nh;
+    ros::NodeHandle nh, priv_nh;
     ros::Subscriber cmd_vel_sub;
     ros::Publisher odom_pub, diag_pub;
     std::string port;
@@ -127,6 +192,8 @@ private:
 
     ros::Time last_diag;
     std::string roboclaw_version;
+
+    tf::TransformBroadcaster br;
 
 };
 
